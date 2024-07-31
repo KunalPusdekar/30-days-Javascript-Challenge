1cf1d6d6195a04cb79e7475e4baad99bb44906a5
KunalPusdekar <125309149+KunalPusdekar@users.noreply.github.com>
2024-07-31 09:22:15 +0530

    Create person.js

---
+++ Day13.js/Activity 1/person.js
@@ -0,0 +1,16 @@
+//Task 2
+
+const person ={
+    age:18,
+    name:"harsh",
+
+    getName(){
+        console.log(`my name is ${this.name}`);
+    },
+
+    getAge(){
+     console.log(`my age is ${this.age}`)
+    }
+}
+
+module.exports = person;
